83836dbaabfbd2ef55fcaf86e15bc0d4c14b2df2
pl23022 <pl23022@ues.edu.sv>
2025-11-27 16:19:51 -0600

    Update reporte_sistema.sh

--- scripts/reporte_sistema.sh
+++ scripts/reporte_sistema.sh
@@ -3,6 +3,6 @@ echo "Fecha y hora:$(timedatectl)"
 echo "Nombre del host:$(hostname)"
 echo "Usuarios conectados:$(who)"
 echo "Espacio disponible:$(df -h)"
-echo "Momoria disponible:$(free -h)"
+echo "Memoria disponible:$(free -h)"
 echo "Contenedores docker:$(docker ps -a)"
 
